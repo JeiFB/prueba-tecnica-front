9bdfac8657f2c8b9d9412b993ead0d0acd1a1cef
jeifher fuentes <fuentesjeifer@gmail.com>
2025-07-23 07:35:53 -0500

    Se refactoriza logica de los layouts en su propio folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,42 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    loadComponent: () =>
-      import('./features/auth/components/login/login.component').then(m => m.LoginComponent)
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./features/auth/components/login/login.component').then(m => m.LoginComponent)
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./features/auth/components/register/register.component').then(m => m.RegisterComponent)
+      },
+    ]
   },
   {
-    path: 'register',
-    loadComponent: () =>
-      import('./features/auth/components/register/register.component').then(m => m.RegisterComponent)
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'tasks',
+        loadChildren: () =>
+          import('./features/tasks/tasks.module').then(m => m.TasksModule),
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
+      },
+    ]
   },
-  {
-    path: 'tasks',
-    loadChildren: () =>
-      import('./features/tasks/tasks.module').then(m => m.TasksModule),
-    canActivate: [authGuard] // Proteger esta ruta
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
-    canActivate: [authGuard] // Proteger también el dashboard
-  },
-  // La ruta raíz debería redirigir a una página protegida si el usuario ya está logueado
   { path: '', redirectTo: 'tasks', pathMatch: 'full' },
-  // Atrapar todo lo demás y redirigir a login
   { path: '**', redirectTo: 'login' }
 ];
